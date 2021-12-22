2f864c97c511f2af9534a949d37e2bc8a312b501
nnunev <nickolaynunev@gmail.com>
2021-12-22 13:25:11 +0200

    starting first service

---
+++ cartridges/star_wars/cartridge/controllers/Star.js
@@ -0,0 +1,40 @@
+'use strict';
+/**
+ * @namespace Star
+ *
+ */
+var server = require('server');
+
+var cache = require('*/cartridge/scripts/middleware/cache');
+
+
+/**
+ * Star-Wars : This will return some star wars info
+ * @name Cat-Fact
+ * @function
+ * @memberof Page
+ * @param {middleware} - server.middleware.include
+ * @param {category} - non-sensitive
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
+server.get(
+    'Wars',
+    server.middleware.include,
+    cache.applyDefaultCache,
+    function (req, res, next) {
+
+        var httpClient = new dw.net.HTTPClient();
+        httpClient.open('GET', 'https://swapi.dev/api/starships/9/');
+        httpClient.setTimeout(3000);
+        httpClient.send();
+
+        var starWars = JSON.parse(httpClient.text);
+
+        res.render('star', { starWars: starWars });
+
+        next();
+    }
+);
+
+module.exports = server.exports();
